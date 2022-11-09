6e153bfeaa221710aaa20e18a2261e3c2618828e
Rebecca Lain <rebecca.lainphotos@gmail.com>
2022-11-09 13:56:54 +0200

    Update to basic directory structure

---
+++ typescript/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+//Normal Javascript
+let foo = 5;
+//Typescript
+let fooy = 5;
+//Typescript immediatly gives VSCode an error: Type 'string' is not assignable to type 'number'.ts
+//fooy = "5";
+console.log("Fooy", fooy);
